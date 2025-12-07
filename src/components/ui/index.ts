cdebc36321d77205fe73392d2b0c7f34264b1002
sdosku20 <sdosku02@gmail.com>
2025-12-07 17:37:24 +0100

    Fix: Clear ui/index.ts to remove deleted component exports

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,16 +1,2 @@
-// Metric Components
-export { MetricHeader } from "./MetricHeader";
-export { MetricIcon } from "./MetricIcon";
-export { MetricBadge } from "./MetricBadge";
-export { MetricValue } from "./MetricValue";
-export { MetricSubtitle } from "./MetricSubtitle";
-export { MetricCard } from "./MetricCard";
-
-// Chart Components
-export { ChartCardHeader } from "./ChartCardHeader";
-export { ChartCardContent } from "./ChartCardContent";
-export { AdvancedChartCard } from "./AdvancedChartCard";
-export { KPICard } from "./KPICard";
-
-// Other UI Components
-export { DateRangeSelector } from "./DateRangeSelector";
+// UI Components - Empty (components removed for simplified chat interface)
+// This file is kept to avoid import errors but no longer exports anything
